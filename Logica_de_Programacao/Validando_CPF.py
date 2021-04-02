9d69bb3444b1854e5af4b7a36cd0b203347e595f
Karinne <karinnecristinapereira@gmail.com>
2021-04-02 14:49:54 -0300

    Exercicios propostos modulo 1

--- Logica_de_Programacao/Validando_CPF.py
+++ Logica_de_Programacao/Validando_CPF.py
@@ -28,27 +28,29 @@ while True:
         break
 
     validador = CPF[:-2] # Obtendo os 9 primeiros dígitos
-    reverso = 10
+    reverso = 10         # Contador reverso
     total = 0
    
     # Faz todo o cálculo: 
     for index in range(19):
-        if index > 8:
-            index -= 9
+        if index > 8:  # Primeiro índice vai de 0 a 9
+            index -= 9 # São os 9 primeiros dígitos do CPF
 
-        total += int(validador[index]) * reverso
+        total += int(validador[index]) * reverso # Valor total da multiplicação
         
-        reverso -= 1
+        reverso -= 1 # Decrementa o contador reverso
         if reverso < 2:
             reverso = 11
             digito = 11 - (total % 11)
-            if digito > 9:
+            if digito > 9: # Se o dígito for maior que 9 o valor é 0
                 digito = 0
-            total = 0
-            validador += str(digito)
+            total = 0 # Zera o total 
+            validador += str(digito) # Concatena o dígito gerado no novo CPF
 
-    # Verificando se o CPF é válido:
-    if CPF == validador:
+    # Se for digitado uma sequêcia como 11111111111 ou 00000000000, ......
+    # O CPF será considerado válido, então vamos tratar esse problema:
+    sequencia = validador == str(validador[0]) * len(CPF)
+    if CPF == validador and not sequencia:
         print('Esse CPF é válido.')
     else:
         print('Esse CPF não é válido.')
